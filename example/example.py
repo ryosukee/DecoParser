ec2f4bcf3343d96bfab4b5019ac2b1fde0919400
ryosukee <tmcit.miyazaki@gmail.com>
2015-12-01 11:53:08 +0900

    [add] file option

--- example/example.py
+++ example/example.py
@@ -10,4 +10,13 @@ def hoge(age):
     print(age, not_main.get_name())
 
 
+@decoparser.option('--f', type=decoparser.FileType())
+def fuga(f):
+    global gf
+    gf = f
+    print('f: ', f.closed)
+
+gf = None
 hoge()
+fuga()
+print('gf: ', gf.closed)
